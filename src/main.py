0849b6e0afbaaf900ba9041956bbd52c98eabbf4
Abhinavkrishnan <abhinavkrishnan36@gmail.com>
2024-10-16 22:40:32 +0530

    Initial project setup with structure and files

---
+++ src/main.py
@@ -0,0 +1,6 @@
+ def main():
+    print("Welcome to PRODIGY GA Project")
+
+if __name__ == "__main__":
+    main()
+
---
+++ src/other_modules.py
@@ -0,0 +1,3 @@
+ def helper_function():
+    print("This is a helper function.")
+
